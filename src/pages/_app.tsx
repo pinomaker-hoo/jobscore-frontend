1f54caf7c8f05d59dca27f3246de0ab2f7feb9be
pinomaker <56928532+pinomaker-hoo@users.noreply.github.com>
2023-04-18 23:14:47 +0900

    feat : base style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
+// ** Style Imports
 import '@/styles/globals.css'
+
+// ** Next Imports
 import type { AppProps } from 'next/app'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <div className="inner_body">
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default App
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
+// ** Next Imports
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 const Home = () => {
   return (
