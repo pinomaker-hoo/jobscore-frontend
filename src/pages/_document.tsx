e57f37f93f3f94e162021bf63447d34bab397f3f
pinomaker <56928532+pinomaker-hoo@users.noreply.github.com>
2023-04-18 23:34:55 +0900

    feat : Head Set

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 const Document = () => {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <title>Jobs Score</title>
+        <meta name="description" content="Jobs Score" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
