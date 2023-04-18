80b24b571fbca67709829b9496f4072e6a752a5b
pinomaker <56928532+pinomaker-hoo@users.noreply.github.com>
2023-04-18 23:21:37 +0900

    feat : Set RTK Config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,20 @@
 // ** Style Imports
 import '@/styles/globals.css'
 
+// ** Redux Imports
+import { Provider } from 'react-redux'
+import { store } from '@/store'
+
 // ** Next Imports
 import type { AppProps } from 'next/app'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <div className="inner_body">
-      <Component {...pageProps} />
-    </div>
+    <Provider store={store}>
+      <div className="inner_body">
+        <Component {...pageProps} />
+      </div>
+    </Provider>
   )
 }
 
