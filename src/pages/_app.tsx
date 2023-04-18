5d9a9e471ad28e873705acaa0e7f6a60cffae2a0
pinomaker <56928532+pinomaker-hoo@users.noreply.github.com>
2023-04-18 23:24:17 +0900

    feat : Mui Style Set

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 // ** Style Imports
 import '@/styles/globals.css'
+import { ThemeProvider } from '@mui/material/styles'
+import { GlobalStyles } from '@mui/material'
+import CssBaseline from '@mui/material/CssBaseline'
+import theme from '@/theme'
 
 // ** Redux Imports
 import { Provider } from 'react-redux'
@@ -11,9 +15,11 @@ import type { AppProps } from 'next/app'
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
-      <div className="inner_body">
+      <ThemeProvider theme={theme}>
+        <GlobalStyles styles={{ backgroundColor: 'blue' }} />
+        <CssBaseline />
         <Component {...pageProps} />
-      </div>
+      </ThemeProvider>
     </Provider>
   )
 }
