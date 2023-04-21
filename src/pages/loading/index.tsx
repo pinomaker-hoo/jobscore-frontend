1833cc82ea61f3efb6352d646535a7a15b70e27a
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-04-22 02:30:26 +0900

    fix : Start Loading View and url

--- src/pages/loading/index.tsx
+++ src/pages/loading/index.tsx
@@ -7,20 +7,13 @@ import { Button, Grid } from '@mui/material'
 const LoadingPage = () => {
   return (
     <Grid container spacing={6}>
-      <Grid item xs={1} />
-      <Grid item xs={10}>
-        <Link href="/loading">
-          <Button
-            variant="contained"
-            size="large"
-            sx={{ backgroundColor: '#F4BC00' }}
-            fullWidth
-          >
+      <Grid item xs={12} sx={{ textAlign: 'center', mt: 85 }}>
+        <Link href="/company">
+          <Button variant="contained" size="large" sx={{ width: '80%' }}>
             설문 정보 가져오는 중...
           </Button>
         </Link>
       </Grid>
-      <Grid item xs={1} />
     </Grid>
   )
 }
