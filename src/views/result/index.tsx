d6e143126c822fe79b2f56dbb4b4c30b60c31f6f
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-05-15 23:54:57 +0900

    fix : Result Page View

--- src/views/result/index.tsx
+++ src/views/result/index.tsx
@@ -650,24 +650,6 @@ const ResultPageView = () => {
               </Grid>
             </Card>
           </Grid>
-          <Grid item xs={12} sx={{ textAlign: 'center', mt: 2 }}>
-            <Button
-              variant="contained"
-              sx={{ width: '80%', backgroundColor: '#6176FF' }}
-            >
-              나와 잘 맞는 회사 알아보기
-            </Button>
-          </Grid>
-          <Grid item xs={12} sx={{ textAlign: 'center', my: -1 }}>
-            <Link href="/rank">
-              <Button
-                variant="contained"
-                sx={{ width: '80%', backgroundColor: '#6176FF' }}
-              >
-                다른 회사 잡스코어 점수 보기
-              </Button>
-            </Link>
-          </Grid>
           <Grid item xs={12} sx={{ textAlign: 'center' }}>
             <Link href="/">
               <Button
@@ -675,10 +657,37 @@ const ResultPageView = () => {
                 sx={{ width: '80%', backgroundColor: '#6176FF' }}
               >
                 다시 테스트 하러 가기
+                <img
+                  src="/common/hand.png"
+                  style={{ marginLeft: '10px', width: '10%' }}
+                />
               </Button>
             </Link>
           </Grid>
-          <Grid item xs={12} sx={{ textAlign: 'center', mb: 5 }}>
+          <Grid item xs={12} sx={{ textAlign: 'center', my: -1 }}>
+            <Link href="/rank">
+              <Button
+                variant="contained"
+                sx={{ width: '80%', backgroundColor: '#6176FF' }}
+              >
+                다른 회사 잡스코어
+                <img
+                  src="/common/hand.png"
+                  style={{ marginLeft: '10px', width: '10%' }}
+                />
+              </Button>
+            </Link>
+          </Grid>
+          <Grid item xs={12} sx={{ textAlign: 'center' }}>
+            <Button
+              variant="contained"
+              disabled
+              sx={{ width: '80%', backgroundColor: '#6176FF' }}
+            >
+              나와 잘 맞는 회사 알아보기 (준비중)
+            </Button>
+          </Grid>
+          <Grid item xs={12} sx={{ textAlign: 'center', mb: 15 }}>
             <Typography variant="h1" sx={{ fontSize: 12 }}>
               Job Score :)
             </Typography>
