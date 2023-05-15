a9d1a3e3400c8e3631af7b395718533a557cbb1b
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-05-16 00:06:14 +0900

    feat : Add Image File

--- src/views/rank/index.tsx
+++ src/views/rank/index.tsx
@@ -1,4 +1,22 @@
+// ** Mui Imports
+import { Grid } from '@mui/material'
+
+// ** Other View Imports
+import WalkingDuck from '@/components/duck/walkingDuck'
+
 const RankPageView = () => {
-  return <h1>Hello world</h1>
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={4.5}>
+        <img src="/common/rankLogo.png" height={80} />
+      </Grid>
+      <Grid item xs={7.5} sx={{ mt: 1.5 }}>
+        <WalkingDuck />
+      </Grid>
+      <Grid item xs={12} sx={{ overflow: 'scroll', maxHeight: '730px' }}>
+        <Grid container spacing={3}></Grid>
+      </Grid>
+    </Grid>
+  )
 }
 export default RankPageView
