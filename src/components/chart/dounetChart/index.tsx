04c071724641fc00dacd53f09b8c7645325bb4f4
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-05-21 23:05:34 +0900

    fix : Donuet Chart Style

--- src/components/chart/dounetChart/index.tsx
+++ src/components/chart/dounetChart/index.tsx
@@ -1,4 +1,5 @@
 // ** Chart Imports
+import { Grid, Typography } from '@mui/material'
 import ReactApexChart from '../apexcharts'
 import { ApexOptions } from 'apexcharts'
 
@@ -7,75 +8,105 @@ interface DounetChartProps {
   series: number
 }
 
-const DounetChart = ({ height, series }: DounetChartProps) => {
-  const options: ApexOptions = {
-    chart: {
-      type: 'radialBar',
-      offsetY: -10,
-      sparkline: {
-        enabled: true,
-      },
-    },
-    plotOptions: {
-      radialBar: {
-        startAngle: -110,
-        endAngle: 110,
-        hollow: {
-          margin: 0,
-          size: '50%',
-          background: 'none',
-          position: 'front',
-          dropShadow: {
-            enabled: true,
-            top: 3,
-            left: 0,
-            blur: 4,
-            opacity: 0.24,
-          },
-        },
-        track: {
-          background: '#e7e7e7',
-          strokeWidth: '90%',
-          margin: 5,
-          dropShadow: {
-            enabled: true,
-            top: 2,
-            left: 0,
-            color: '#999',
-            opacity: 1,
-            blur: 2,
-          },
-        },
-      },
-    },
-    grid: {
-      padding: {
-        top: -10,
-      },
-    },
-    fill: {
-      type: 'gradient',
-      colors: ['#C1C3F8', '#A39FFF', '#6176FF'],
-      gradient: {
-        shade: 'light',
-        shadeIntensity: 0.4,
-        inverseColors: false,
-        opacityFrom: 1,
-        opacityTo: 1,
-        stops: [0, 50, 100],
-      },
-    },
-    labels: [''],
-  }
+const DounetChart = () => {
+  // const options: ApexOptions = {
+  //   chart: {
+  //     type: 'radialBar',
+  //     offsetY: -10,
+  //     sparkline: {
+  //       enabled: true,
+  //     },
+  //   },
+  //   plotOptions: {
+  //     radialBar: {
+  //       startAngle: -110,
+  //       endAngle: 110,
+  //       hollow: {
+  //         margin: 0,
+  //         size: '50%',
+  //         background: 'none',
+  //         position: 'front',
+  //         dropShadow: {
+  //           enabled: true,
+  //           top: 3,
+  //           left: 0,
+  //           blur: 4,
+  //           opacity: 0.24,
+  //         },
+  //       },
+  //       track: {
+  //         background: '#e7e7e7',
+  //         strokeWidth: '90%',
+  //         margin: 5,
+  //         dropShadow: {
+  //           enabled: true,
+  //           top: 2,
+  //           left: 0,
+  //           color: '#999',
+  //           opacity: 1,
+  //           blur: 2,
+  //         },
+  //       },
+  //     },
+  //   },
+  //   grid: {
+  //     padding: {
+  //       top: -10,
+  //     },
+  //   },
+  //   fill: {
+  //     type: 'solid',
+  //     colors: ['#C1C3F8', '#A39FFF', '#6176FF'],
+  //     // gradient: {
+  //     //   shade: 'light',
+  //     //   shadeIntensity: 0.4,
+  //     //   inverseColors: false,
+  //     //   opacityFrom: 1,
+  //     //   opacityTo: 1,
+  //     //   stops: [0, 50, 100],
+  //     // },
+  //   },
+  //   labels: [''],
+  // }
 
   return (
-    <ReactApexChart
-      series={[series]}
-      height={height}
-      type="radialBar"
-      toolbar={{ show: true }}
-      options={options}
-    />
+    <Grid
+      container
+      sx={{
+        position: 'relative',
+      }}
+    >
+      <Grid item xs={12}>
+        <img src="/chart/pie.png" style={{ width: '100%' }} />
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        sx={{
+          position: 'absolute',
+          transform: 'translate(-50%, 10%) translateY(100%)',
+          left: '50%',
+        }}
+      >
+        <img src="/chart/pin.png" />
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        sx={{
+          position: 'absolute',
+          transform: 'translate(-50%, 10%) translateY(100%)',
+          left: '50%',
+        }}
+      >
+        <Typography variant="body1" sx={{ fontSize: 50 }}>
+          736
+        </Typography>
+        <Typography variant="body1" sx={{ fontSize: 20, color: '#3A50DE' }}>
+          높음
+        </Typography>
+      </Grid>
+    </Grid>
   )
 }
 
