ec966b3faf9813c8dd4e05efdbf555ca600b3d59
pinomaker-hoo <inhoo23@naver.com>
2023-07-08 23:14:20 +0900

    fix : Og Image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,13 +5,17 @@ const Document = () => {
     <Html lang="ko">
       <Head>
         <title>Jobs Score</title>
-        <meta name="description" content="Jobs Score" />
+        <meta
+          name="description"
+          content="Jobs Score, 우리 회사의 잡스코어는? 우리회사와 내가 원하는 회사의 점수를 비교해보세요!"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script
           type="text/JavaScript"
           src="https://developers.kakao.com/sdk/js/kakao.min.js"
         />
         <link rel="icon" href="/favicon.png" />
+        <meta property="og:image" content="/og.png" />
       </Head>
       <body>
         <Main />
